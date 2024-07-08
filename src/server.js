410aa5565fabe25bdc82000aef299fd1c8f416fc
Alexandr-1973 <144156771+Alexandr-1973@users.noreply.github.com>
2024-07-08 03:22:55 +0300

    mid

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
+// import { TEMP_UPLOAD_DIR } from './constants/index.js';
 
 dotenv.config();
 
@@ -26,6 +28,9 @@ export const setupServer = () => {
   );
 
   app.use(cookieParser());
+
+  // app.use('/uploads', express.static(TEMP_UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use(router);
 
